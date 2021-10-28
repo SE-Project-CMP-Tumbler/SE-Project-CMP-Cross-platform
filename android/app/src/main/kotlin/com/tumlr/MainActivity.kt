3ab29cad659b70367ce8e07a4d9270cc20f8dc36
Ziyad Hassan <ziyadhassan500@gmail.com>
2021-10-28 22:08:23 +0200

    Initial Code

---
+++ android/app/src/main/kotlin/com/tumlr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tumlr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() =>  runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("The Start");
+  }
+}
