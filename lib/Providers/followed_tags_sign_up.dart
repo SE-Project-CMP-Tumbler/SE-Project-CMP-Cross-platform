665ed8680e5ed3a7ad94bbbee57691e88ba8198f
Donia Esawi <14712018101273@stud.cu.edu.eg>
2021-11-17 10:04:32 +0200

    add post button

--- lib/Providers/followed_tags_sign_up.dart
+++ lib/Providers/followed_tags_sign_up.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class FollowedTags with ChangeNotifier {
-  Set<String> _followedTags;
+  //Set<String> _followedTags;
 
-  Set<String> get followedTags => _followedTags;
+  //Set<String> get followedTags => _followedTags;
 
   FollowedTags() {
-    _followedTags = {};
+    return _followedTags = {};
   }
 
   void addFollowTag(String tag) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:tumlr/Providers/followed_tags_sign_up.dart';
-import 'package:tumlr/Screens/Sign_Up_Screens/Choose_Tag/tag_page.dart';
+import 'package:tumbler/Providers/followed_tags_sign_up.dart';
+import 'package:tumbler/Screens/Sign_Up_Screens/Choose_Tag/tag_page.dart';
+import 'package:tumbler/Screens/Sign_Up_Screens/intro_screens/on_start_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,12 +12,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
+    /**return ChangeNotifierProvider(
       create: (_) => FollowedTags(),
       child: const MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: TagSelect(),
+        home: OnStart(),
       ),
+    );**/
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: OnStart(),
     );
   }
 }
