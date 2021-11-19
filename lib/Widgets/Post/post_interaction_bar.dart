b3f672a093d211bc4ea86a9b1f294d5fc4a951e6
Ahmed Waleed <62077516+Ahmed-walid@users.noreply.github.com>
2021-11-19 14:47:09 +0200

    Adding love comment reblog icons beside notes number

--- lib/Widgets/Post/post_interaction_bar.dart
+++ lib/Widgets/Post/post_interaction_bar.dart
@@ -13,55 +13,54 @@ class _PostInteractionBarState extends State<PostInteractionBar> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            Container(
-              width: 50,
-              child: FlatButton(
-                  onPressed: () {},
-                  child: Image.asset(
-                    'assets/images/icons8-love-circled-50.png',
-                    fit: BoxFit.cover,
-                  )),
-            ),
-            Expanded(
-              child: Text(
-                '${widget.notesNum} notes',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
-                  color: Colors.black87,
-                ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            width: 60,
+            child: FlatButton(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+                onPressed: () {},
+                child: Image.asset(
+                  'assets/images/interactions.jpeg',
+                )),
+          ),
+          Expanded(
+            child: Text(
+              '${widget.notesNum} notes',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+                color: Colors.black87,
               ),
             ),
-            const IconButton(
-                onPressed: null,
-                icon: Icon(
-                  Icons.reply,
-                  color: Colors.black,
-                )),
-            const IconButton(
-                onPressed: null,
-                icon: Icon(
-                  Icons.repeat,
-                  color: Colors.black,
-                )),
-            const IconButton(
-                onPressed: null,
-                icon: Icon(
-                  Icons.favorite_border_outlined,
-                  color: Colors.black,
-                )),
-            const IconButton(
-                onPressed: null,
-                icon: Icon(
-                  Icons.add_comment_outlined,
-                  color: Colors.black,
-                )),
-          ],
-        ),
+          ),
+          const IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.reply,
+                color: Colors.black,
+              )),
+          const IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.repeat,
+                color: Colors.black,
+              )),
+          const IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.favorite_border_outlined,
+                color: Colors.black,
+              )),
+          const IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.add_comment_outlined,
+                color: Colors.black,
+              )),
+        ],
       ),
     );
   }
