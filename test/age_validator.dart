0043eb4c7c095affec2c7604759c99b31dfc4ac8
Ziyad Hassan <ziyadhassan500@gmail.com>
2021-11-20 23:34:11 +0200

    age testing

---
+++ test/age_validator.dart
@@ -0,0 +1,26 @@
+import "package:flutter_test/flutter_test.dart";
+import "package:tumbler/Methods/age_validation.dart";
+
+void main() {
+  test("Empty age string", () {
+    final String? result = ageValidator("");
+    expect(result, "Please Enter a number");
+  });
+
+  test("Entering Text", () {
+    final String? result = ageValidator("abc");
+    expect(result, "Please Enter a Valid Number");
+  });
+
+  test("Entering age out of the boundaries", () {
+    String? result = ageValidator("12");
+    expect(result, "Please Enter age between 13 and 130");
+    result = ageValidator("131");
+    expect(result, "Please Enter age between 13 and 130");
+  });
+
+  test("Entering correct age", () {
+    final String? result = ageValidator("50");
+    expect(result, null);
+  });
+}
