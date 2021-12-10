764cab86eb14e8fa7abd4f1bf71e0f319a681bc1
Donia Esawi <14712018101273@stud.cu.edu.eg>
2021-12-10 16:08:04 +0200

    Profile

---
+++ lib/Widgets/Post/personal_post_interaction_bar.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:like_button/like_button.dart';
+/// same as post intercation bar but with som additional features
+class PersonalPostInteractionBar extends StatefulWidget {
+  /// constructor, takes the notes number
+  const PersonalPostInteractionBar(
+      {required final this.notesNum,
+        final Key? key,})
+      : super(key: key);
+  /// count of notes to display on the post
+  final int? notesNum;
+  @override
+  _PersonalPostInteractionBarState createState() =>
+      _PersonalPostInteractionBarState();
+}
+
+class _PersonalPostInteractionBarState extends State<PersonalPostInteractionBar>
+{
+  bool isLoved = false;
+  NumberFormat numFormatter = NumberFormat.decimalPattern("en_us");
+
+  @override
+  Widget build(final BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            TextButton(
+              onPressed: (){},
+              child: Text(
+                "${numFormatter.format(widget.notesNum)} notes",
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Colors.black45,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    CupertinoIcons.arrowshape_turn_up_right,
+                    color: Colors.grey.shade800,
+                  ),
+                  iconSize: 20,
+                ),
+                IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    CupertinoIcons.conversation_bubble,
+                    color: Colors.grey.shade800,
+                  ),
+                  iconSize: 20,
+
+                ),
+                IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.repeat,
+                    color: Colors.grey.shade800,
+                  ),
+                  iconSize: 20,
+                ),
+                LikeButton(
+                  isLiked: isLoved,
+                  likeBuilder: (final bool isLoved) {
+                    final Color color = isLoved ? Colors.red : Colors.grey.shade800;
+                    return Icon(
+                      isLoved ? Icons.favorite : Icons.favorite_border_outlined,
+                      color: color,
+                      size: 20,
+                    );
+                  },
+                ),
+                IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    CupertinoIcons.trash,
+                    color: Colors.grey.shade800,
+                  ),
+                  iconSize: 20,
+                ),
+                IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Icons.edit_outlined,
+                    color: Colors.grey.shade800,
+                  ),
+                  iconSize: 20,
+                ),
+              ],
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Widgets/Post/profile_personal_post.dart
@@ -0,0 +1,137 @@
+import "package:flutter/cupertino.dart";
+import "package:flutter/material.dart";
+import "package:tumbler/Constants/colors.dart";
+import 'package:tumbler/Models/post.dart';
+import 'package:tumbler/Screens/Home_Page/home_page.dart';
+import 'package:tumbler/Screens/Profile/profile_page.dart';
+import 'package:tumbler/Widgets/Post/personal_post_interaction_bar.dart';
+import 'package:tumbler/Widgets/Post/post_top_bar.dart';
+/// for the 'Posts' tab in ProfilePage
+class PersonalPost extends StatefulWidget {
+  /// constructor
+  ///takes
+  ///*showEditPostBottomSheet function that responsible for showing
+  ///some options about the post by clicking on more vert icon
+  ///*Post model contains all data about the post
+  const PersonalPost({
+    required final this.showEditPostBottomSheet,
+    required final this.post,
+    final Key? key,
+  }) : super(key: key);
+
+  /// to be passed to [PostTopBar]
+  final Function showEditPostBottomSheet;
+
+  /// The Content of the Post
+  final Post post;
+
+  @override
+  _PersonalPostState createState() => _PersonalPostState();
+}
+
+class _PersonalPostState extends State<PersonalPost> {
+  @override
+  Widget build(final BuildContext context) {
+    final double _width = MediaQuery.of(context).size.width;
+    final double _height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: navy,
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // tumbler user name and profile pic and options
+              PostTopBar(
+                showEditPostBottomSheet: showEditPostProfileBottomSheet,
+                avatarPhotoLink: widget.post.blogAvatar,
+                name: widget.post.blogUsername,
+              ),
+              Image.asset("assets/images/cat.png", width: _width,fit: BoxFit.fitWidth,),
+              PersonalPostInteractionBar(notesNum: 11),
+              /**Padding(
+                padding: const EdgeInsets.all(6),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    IconButton(
+                      onPressed: (){},
+                      icon: Icon(CupertinoIcons.arrowshape_turn_up_right,
+                        color: Colors.grey.shade800,size: 22,
+                      ),
+                      constraints: BoxConstraints.tight(
+                          const Size(32,40),
+                      ),
+                    ),
+                    const SizedBox(width: 8,),
+                    IconButton(
+                      onPressed: (){},
+                      icon: Icon(CupertinoIcons.chat_bubble,
+                        color: Colors.grey.shade800,size: 22,
+                      ),
+                      constraints: BoxConstraints.tight(
+                        const Size(32,40),
+                      ),
+
+                    ),
+                    const SizedBox(width: 8,),
+
+                    IconButton(
+                      onPressed: (){},
+                      icon: Icon(Icons.repeat,
+                        color: Colors.grey.shade800,size: 22,
+                      ),
+                      constraints: BoxConstraints.tight(
+                        const Size(32,40),
+                      ),
+
+                    ),
+                    const SizedBox(width: 8,),
+
+                    IconButton(
+                      onPressed: (){},
+                      icon: Icon(CupertinoIcons.heart,
+                        color: Colors.grey.shade800,size: 22,
+                      ),
+                      constraints: BoxConstraints.tight(
+                        const Size(32,40),
+                      ),
+                    ),
+                    const SizedBox(width: 8,),
+
+                    IconButton(
+                      onPressed: (){},
+                      icon: Icon(CupertinoIcons.delete,
+                        color: Colors.grey.shade800,size: 22,
+                      ),
+                      constraints: BoxConstraints.tight(
+                        const Size(32,40),
+                      ),
+                    ),
+                    const SizedBox(width: 8,),
+
+                    IconButton(
+                      onPressed: (){},
+                      icon: Icon(Icons.edit_outlined,
+                        color: Colors.grey.shade800,size: 22,
+                      ),
+                      constraints: BoxConstraints.tight(
+                        const Size(32,40),
+                      ),
+
+                    ),
+                    const SizedBox(width: 16,),
+
+
+                  ],
+                ),
+              ),**/
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
