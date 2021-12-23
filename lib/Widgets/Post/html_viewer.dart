df3048723820b0fb8892824ab06efc053a9b744e
Ziyad Hassan <ziyadhassan500@gmail.com>
2021-12-23 19:48:39 +0200

    Update html_viewer.dart

--- lib/Widgets/Post/html_viewer.dart
+++ lib/Widgets/Post/html_viewer.dart
@@ -1,7 +1,33 @@
+import "package:csslib/src/messages.dart";
 import "package:flutter/material.dart";
 import "package:flutter_html/flutter_html.dart";
 
-///Used to render html content
+String process(final String htmlBeforeProcessing) {
+  // Getting all images
+  String html = htmlBeforeProcessing;
+  int index1 = 0;
+  int x = 0;
+  while (x != -1 && index1 <= html.length - 5) {
+    x = html.indexOf("@", index1);
+    print("here3 $x");
+    if (x != -1) {
+      index1 = html.indexOf(" ", x);
+      if (index1 == -1)
+        index1 = html.indexOf("<", x);
+
+      print("here4 $index1");
+      String mention = html.substring(x,index1);
+      html = html.replaceRange(x, index1, "<a href='post'>$mention</a>");
+      print(html);
+      index1 += 18;
+      print("index = ${html[index1-1]}");
+    }
+  }
+
+  return html;
+}
+
+/// Used to render html content
 class HtmlView extends StatelessWidget {
   /// takes html content as string.
   const HtmlView({
@@ -15,7 +41,7 @@ class HtmlView extends StatelessWidget {
   @override
   Widget build(final BuildContext context) {
     return Html(
-      data: htmlData,
+      data: process(htmlData),
       style: <String, Style>{
         "table": Style(
           backgroundColor: const Color.fromARGB(0x50, 0xee, 0xee, 0xee),
@@ -55,24 +81,26 @@ class HtmlView extends StatelessWidget {
         "img": Style(alignment: Alignment.center)
       },
       // ignore: always_specify_types
-      // customImageRenders: {
-      //   (final Map<String, String> attr, final __) => attr["src"] != null:
-      //       networkImageRender(width: 500, height: 300),
-      // },
-      // onLinkTap: (final String? url, final _, final __, final ___) {},
-      // onImageTap: (final String? src, final _, final __, final ___) {
-      //   //print(src);
-      // },
-      // onImageError: (final Object exception, final StackTrace? stackTrace) {
-      //   //print(exception);
-      // },
-      // onCssParseError: (final String css, final List<Message> messages) {
-      //   //print("css that error: $css");
-      //   //print("error messages:");
-      //   // for (var element in messages) {
-      //   //   //print(element);
-      //   // }
-      // },
+      customImageRenders: {
+        (final Map<String, String> attr, final __) => attr["src"] != null:
+            networkImageRender(width: 500, height: 300),
+      },
+      onLinkTap: (final String? url, final _, final __, final ___) {
+        print("link is clicked");
+      },
+      onImageTap: (final String? src, final _, final __, final ___) {
+        //print(src);
+      },
+      onImageError: (final Object exception, final StackTrace? stackTrace) {
+        //print(exception);
+      },
+      onCssParseError: (final String css, final List<Message> messages) {
+        //print("css that error: $css");
+        //print("error messages:");
+        // for (var element in messages) {
+        //   //print(element);
+        // }
+      },
     );
   }
 }
